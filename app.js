5f6c1549077612a27ee1374d9b417b530f954f4f
Bao Anh Le <anh.lb194470@gmail.com>
2023-06-03 15:32:59 +0700

    adding cors

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const cors = require("cors");
 const app = express();
 require("./models");
 const roomRouter = require("./routes/roomRoutes");
@@ -9,6 +10,7 @@ dotenv.config({
   path: "./config.env",
 });
 
+app.use(cors());
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
 
