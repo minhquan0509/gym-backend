026d2ff08135ad7062e671ccc2edd4ec834cb250
Bao Anh Le <anh.lb194470@gmail.com>
2023-06-03 14:51:40 +0700

    gym room api

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,15 +1,22 @@
 const { User, sequelize, QueryTypes } = require("../models/index");
 
 exports.getAllUsers = async (req, res, next) => {
-  const users = await User.findAll({ attributes: { exclude: ["password"] } });
-  // const users = await sequelize.query("SELECT * FROM `users`", {
-  //   type: QueryTypes.SELECT,
-  // });
-  res.status(200).json({
-    status: "success",
-    results: users.length,
-    data: {
-      users,
-    },
-  });
+  try {
+    const users = await User.findAll({ attributes: { exclude: ["password"] } });
+    // const users = await sequelize.query("SELECT * FROM `users`", {
+    //   type: QueryTypes.SELECT,
+    // });
+    res.status(200).json({
+      status: "success",
+      results: users.length,
+      data: {
+        users,
+      },
+    });
+  } catch (error) {
+    res.status(404).json({
+      status: "fail",
+      message: error,
+    });
+  }
 };
