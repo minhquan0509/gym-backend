ca6d25d85284bd02bc8df56bed989b1110fa6342
Bao Anh Le <anh.lb194470@gmail.com>
2023-06-22 00:25:48 +0700

    [RoomAPI] fix update api

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ dotenv.config({
 app.use(cors());
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static(`${__dirname}/public`));
 
 // app.use((req, res, next) => {
